d2d67837f09052d5c1d3b7ee3bbc86afa0866b80
ivoronline <ivoronline@gmail.com>
2024-06-26 09:48:18 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_database_loadscript/SpringbootDatabaseLoadscriptApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_database_loadscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseLoadscriptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDatabaseLoadscriptApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_database_loadscript/SpringbootDatabaseLoadscriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_database_loadscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabaseLoadscriptApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
